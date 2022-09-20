3e4d2d4796da0f87b44bbca8bcf3ca7374243352
Connor McCue <connor.mccue@comcast.net>
2022-09-20 15:45:47 -0700

    we got flags!

--- src/components/FlagCard/FlagCard.js
+++ src/components/FlagCard/FlagCard.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-export default function FlagCard({ country, iso2, continent }) {
+export default function FlagCard({ id, name, iso2, iso3, local_name }) {
   return (
     <div className="flag-card">
       <div className="details">
-        <div className="country">{country}</div>
+        <div className="country">{name}</div>
       </div>
       <div className="flag">
-        <img src="https://flagcdn.com/16x12/ad.png"
-          srcSet="https://flagcdn.com/32x24/ad.png 2x,
-          https://flagcdn.com/48x36/ad.png 3x"
+        <img src={`https://flagcdn.com/16x12/${iso2.toLowerCase()}.png`}
+          srcSet={`https://flagcdn.com/32x24/${iso2.toLowerCase()}.png 2x`}
           width="16"
           height="12"
           alt="Andorra" />
