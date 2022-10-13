955c3e951d222ded6f7c9d96ba443f44b850194d
Connor McCue <connor.mccue@comcast.net>
2022-10-12 18:07:30 -0700

    drop down working for all continents

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -10,6 +10,12 @@ export default function Filter({ continent, setContinent }) {
         }}>
         <option value ="all">All</option>
         <option value ="Europe">Europe</option>
+        <option value ="Asia">Asia</option>
+        <option value ="North America">North America</option>
+        <option value ="South America">South America</option>
+        <option value ="Oceania">Oceania</option>
+        <option value ="Africa">Africa</option>
+        <option value ="Antarctica">Antarctica</option>
       </select>
     </>
   );
